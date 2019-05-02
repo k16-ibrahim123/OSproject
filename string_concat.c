940718b6364526e9ffd3261d0e694958d5e2aac4
k16-ibrahim123 <k163832@nu.edu.pk>
2019-05-02 18:30:13 +0500

    first commit

---
+++ string_concat.c
@@ -0,0 +1,85 @@
+
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/types.h> 
+#include<string.h> 
+#include<sys/wait.h> 
+  
+int main() 
+{ 
+    
+  
+    int fd1[2];  // Used to store two ends of first pipe 
+    int fd2[2];  // Used to store two ends of second pipe 
+  
+    char fixed_str[] = "string is"; 
+    char input_str[100]; 
+    pid_t p; 
+  
+    if (pipe(fd1)==-1) 
+    { 
+        fprintf(stderr, "Pipe Failed" ); 
+        return 1; 
+    } 
+    if (pipe(fd2)==-1) 
+    { 
+        fprintf(stderr, "Pipe Failed" ); 
+        return 1; 
+    } 
+  
+    scanf("%s", input_str); 
+    p = fork(); 
+  
+    if (p < 0) 
+    { 
+        fprintf(stderr, "fork Failed" ); 
+        return 1; 
+    } 
+  
+    // Parent process 
+    else if (p > 0) 
+    { 
+        char concat_str[100]; 
+  
+        close(fd1[0]);  // Close reading end of first pipe 
+  
+        
+write(fd1[1], input_str, strlen(input_str)+1); 
+        close(fd1[1]); 
+  
+         
+        wait(NULL); 
+  
+        close(fd2[1]); 
+        read(fd2[0], concat_str, 100); 
+        printf("Concatenated string %s\n", concat_str); 
+        close(fd2[0]); 
+    } 
+ 
+    else
+    { 
+        close(fd1[1]);  
+  
+       
+ 	char concat_str[100]; 
+        read(fd1[0], concat_str, 100); 
+  
+        
+        int k = strlen(concat_str); 
+        int i; 
+        for (i=0; i<strlen(fixed_str); i++) 
+            concat_str[k++] = fixed_str[i]; 
+  
+        concat_str[k] = '\0';   
+  
+        close(fd1[0]); 
+        close(fd2[0]); 
+  
+        write(fd2[1], concat_str, strlen(concat_str)+1); 
+        close(fd2[1]); 
+  
+        exit(0); 
+    } 
+} 
+
